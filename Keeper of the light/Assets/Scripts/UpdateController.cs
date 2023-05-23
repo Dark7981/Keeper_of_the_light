d3e27e22e3f14045ec30cc466562b799ad887a7a
Dark7981 <oleksandrvorobej02@gmail.com>
2023-05-23 16:35:24 +0300

    Рання версія руху камери
    Зробив ранню версію руху камери яку буду дороблювати в суботу

--- Keeper of the light/Assets/Scripts/UpdateController.cs
+++ Keeper of the light/Assets/Scripts/UpdateController.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class UpdateController : MonoBehaviour
 {
-    [SerializeField] private PlayerMovement _playerMovement; // скрипт в якому вкористовується Update()
+    [SerializeField] private PlayerMovement _playerMovement; 
 
     private void Update()
     {
-        _playerMovement.ScriptUpdate();  // визиваю метод який відповідає за Update()
+        _playerMovement.ScriptUpdate(); 
     }
 
     private void FixedUpdate()
     {
-        _playerMovement.ScriptFixedUpdate();  // визиваю метод який відповідає за FixedUpdate()
+        _playerMovement.ScriptFixedUpdate();  
     }
 }
