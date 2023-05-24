12f2f13c88f5ce57453666629251b101d48e686b
Dark7981 <oleksandrvorobej02@gmail.com>
2023-05-24 18:53:23 +0300

    Присідання (рання версія)
    Зробив механіку присіданнь але не зробив її взаємодію зі звуком бо скрипта ще немає

--- Keeper of the light/Assets/Scripts/PlayerMovement.cs
+++ Keeper of the light/Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 4;  
-    [SerializeField] private float rotationSpeed = 10;
 
+    [SerializeField] private KeyCode sitKey = KeyCode.C;
+    [SerializeField] private float moveSpeed = 4;
+    [SerializeField] private float sittingMoveSpeed = 2;
+    [SerializeField] private float rotationSpeed = 10;
+    [SerializeField] private Sprite sittingSprite;  //спрайт персонажу при сидінні
+
+    private Sprite standartSprite;  // спрайт який був на гравці по замовчуванням
+    private float speed = 4;
+    private bool isSiting = false;
     private Vector2 moveDirection;      // напрям руху
-    private Rigidbody2D rigidBody;  
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();   // беру компонент 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        standartSprite = spriteRenderer.sprite;
+
+        if (sittingSprite == null) // цей код щоб під час ваших тестів не було помилок
+        {
+            sittingSprite = standartSprite;
+        }
     }
 
     public void ScriptUpdate()   // це метод який я буду використовувати в UpdateController 
+    {
+        Movement();
+        Siting();
+    }
+
+    private void Siting() // механіка присідання
+    {
+        if (Input.GetKeyDown(sitKey) && isSiting == false) // перевірка чи хочеш сісти або піднятись
+        {
+            isSiting = true;
+
+            speed = sittingMoveSpeed;
+            spriteRenderer.sprite = sittingSprite;
+        
+
+        }
+        else if (Input.GetKeyDown(sitKey) && isSiting == true)  
+        {
+            isSiting = false;
+
+            speed = moveSpeed; 
+            spriteRenderer.sprite = standartSprite;
+        }
+    }
+
+    private void Movement() // відровідає за рух гравця
     {
         moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // Напрям руху записується по осям
 
@@ -30,6 +72,6 @@ public class PlayerMovement : MonoBehaviour
 
     public void ScriptFixedUpdate() // Це метод який я буду використовувати в UpdateController 
     {
-        rigidBody.MovePosition(rigidBody.position + moveDirection * moveSpeed * Time.fixedDeltaTime);  // Це метод який переміщує об'єкт в указану точку з заданою швидкістю
+        rigidBody.MovePosition(rigidBody.position + moveDirection * speed * Time.fixedDeltaTime);  // Це метод який переміщує об'єкт в указану точку з заданою швидкістю
     }
 }
