a0127c5f2a1652d0d600d05ecab0a385f924a6c5
frstydd <ermolaew.brothers@gmail.com>
2023-09-08 20:35:47 +0300

    трохи візуала для головоломок, та деякі допрацбювання

--- Keeper of the light/Assets/Scripts/MiniGame/Golovolomka/PuzzleStones/CellStones.cs
+++ Keeper of the light/Assets/Scripts/MiniGame/Golovolomka/PuzzleStones/CellStones.cs
@@ -7,15 +7,26 @@ using UnityEngine;
 public class CellStones : MonoBehaviour
 {
     [SerializeField] private StoneData _requiredStone;
+    private Animator _animator;
 
     public Action<CellStones> cellActive;
     public Action<CellStones> cellDisActive;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Stone>())
         {
             if (collision.GetComponent<Stone>().GetData() == _requiredStone)
+            {
                 cellActive?.Invoke(gameObject.GetComponent<CellStones>());
+                _animator.SetTrigger("Correct");
+            }else
+                _animator.SetTrigger("Wrong");
+                
+
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
